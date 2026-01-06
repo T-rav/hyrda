c7078140a9eca0e9a9e015a3ab985fa6f9ed28b4
t-rav <travis@8thlight.com>
2026-01-06 14:32:18 -0700

    chore: Remove librechat-custom before subtree merge

--- librechat-custom/client/src/components/InsightMesh/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DeepSearchToggle } from './DeepSearchToggle';
-export { default as ResearchDepthSelector } from './ResearchDepthSelector';
-export { default as AgentSidebar } from './AgentSidebar';
-export { default as AgentCard } from './AgentCard';
